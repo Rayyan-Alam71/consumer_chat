917d333b8f5a0f087f3dd2c5c815f2ed3774a3d7
Rayyan-Alam71 <rayyanalam752@gmail.com>
2025-10-13 02:55:52 +0530

    securing env secrets

--- lib/pinecone.ts
+++ lib/pinecone.ts
@@ -3,7 +3,7 @@
 export async function getPineconeIndex() {
    const { Pinecone } = await import('@pinecone-database/pinecone');
    const pc = new Pinecone({
-      apiKey: process.env.PINECONE_API_KEY || 'pcsk_3tG8NP_K6w9sK4zGDJQ6yR38o6DDoXAjKN4oexFZF58VarYPqiKuV2ySysCZGFLsRMe1T1',
+      apiKey: process.env.PINECONE_API_KEY!
    });
-   return pc.index(process.env.PINECONE_INDEX || 'documind');
+   return pc.index(process.env.PINECONE_INDEX!);
 }
